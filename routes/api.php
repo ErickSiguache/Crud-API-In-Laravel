17f25d9f72816c3dc8ba41c5484420ecfb51b75a
ErickSiguache <erickaguilars403@gmail.com>
2024-03-08 18:06:46 -0600

    feat: add the paths for each endpoint of the client controller queries

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\ClientController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/client', [ClientController::class, "getClients"]);
+Route::get('client/{id}', [ClientController::class, "showClientById"]);
+Route::post('/client', [ClientController::class, "postClient"]);
+Route::put('/client', [ClientController::class, "putClient"]);
+Route::delete('/client/{id}', [ClientController::class, "deleteClient"]);
